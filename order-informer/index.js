fe220715630f1ee9367ba29cc47fd18f0e994d41
Walter Dal Mut <walter.dalmut@gmail.com>
2019-12-24 10:46:26 +0100

    Added base Order informer

--- order-informer/index.js
+++ order-informer/index.js
@@ -8,11 +8,12 @@ kc.loadFromDefault();
 
 const k8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
 
-const informer = k8s.makeInformer(kc, '/apis/app.corley.it/v1/conferences', () => {
-  return k8sApi.listClusterCustomObject('app.corley.it', 'v1', 'conferences')
+const informer = k8s.makeInformer(kc, '/apis/app.corley.it/v1/orders', () => {
+  return k8sApi.listClusterCustomObject('app.corley.it', 'v1', 'orders')
 })
 
 informer.on('add', obj => client.lpush(process.env.ADD_QUEUE_NAME, JSON.stringify(obj), redis.print))
+informer.on('update', obj => client.lpush(process.env.UPDATE_QUEUE_NAME, JSON.stringify(obj), redis.print))
 informer.on('delete', obj => client.lpush(process.env.DELETE_QUEUE_NAME, JSON.stringify(obj), redis.print))
 
 informer.start()
